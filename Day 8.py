d0ab4acbf2d14d6a49b0b3e33fe1b1eed4b96354
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-10-16 19:14:49 -0700

    Python homework day 8

---
+++ Day 8.py
@@ -0,0 +1,14 @@
+print("Let's make the world a bit kinder today!")
+
+hName = input("What's your name awesome human? ")
+if hName == "Sierra" or hName == "sierra":
+    print("Hi Sierra--that's one of my favorite names!")
+
+    goals = input(" I know we all have goals, but do you have one that stands out? ")
+    if "better" in goals:
+        print("That's wonderful! I like the idea of growing and learning as one of my personal goals. ")
+    elif "goal" in goals:
+        print("Goals are always a good think in my book.")
+
+else: print("Hey you know what?  I think you're doing a great job staying positive despite all the challenges you've had.  Thanks for being awesome!")
+
