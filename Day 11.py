5c30add9cb67f99b3429392144904bc08b45d48d
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-10-19 10:08:21 -0700

    Python homework day 11

---
+++ Day 11.py
@@ -0,0 +1,17 @@
+#lets count how many seconds there are in a year!
+#60 seconds in a minute
+#60 minutes in 1 hr
+#24 hrs in a day
+#365 days in a year
+
+minutesInADay = 60 * 24
+print(minutesInADay)
+
+
+secondsInADay = minutesInADay * 60;
+print(secondsInADay)
+
+secondsInAYear = secondsInADay * 365;
+print(secondsInAYear)
+
+
